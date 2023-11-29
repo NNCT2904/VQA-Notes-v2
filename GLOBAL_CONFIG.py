4b11ed9bd6de01c668bfffe35e21fef8982b80a0
ncng <nnct2000@gmail.com>
2023-11-30 00:49:12 +0700

    upgrade to parse pauli op, update notebooks for primitives

--- GLOBAL_CONFIG.py
+++ GLOBAL_CONFIG.py
@@ -1,8 +1,8 @@
-from qiskit.opflow import PauliSumOp
+from qiskit.quantum_info import SparsePauliOp
 
 DATA_SIZE = 500
 FEATURE_DIM = 4
-MAX_ITER = 120
+MAX_ITER = 10
 MAX_INST = 10
 
 MAX_QUBITS = 9
@@ -11,7 +11,7 @@ MIN_REPS = 1
 MAX_IDENTITIES_BLOCKS = 2  # <---- Number of identity blocks, depth values of identity blocks is close to that of normal ansatz (5 qubits)
 ENTANGLEMENT = 'linear'
 
-GLOBAL_OPERATOR = PauliSumOp.from_list([('Z'*MAX_QUBITS, 1)])
-LOCAL_OPERATOR = PauliSumOp.from_list([('I' * (MAX_QUBITS - 2)+'Z'*2, 1)])
+GLOBAL_OPERATOR = SparsePauliOp.from_list([('Z'*MAX_QUBITS, 1)])
+LOCAL_OPERATOR = SparsePauliOp.from_list([('I' * (MAX_QUBITS - 2)+'Z'*2, 1)])
 
 LOG_PATH = './Logs-MNIST-v4'
