52460ca630c43bf8dd52b8244c34a38a66354da2
Thanh Nguyen <nnct2000@gmail.com>
2023-12-29 23:58:42 +0700

    Adjusting Global Config
    12 qubits is way too expensive for emulation

--- GLOBAL_CONFIG.py
+++ GLOBAL_CONFIG.py
@@ -16,6 +16,7 @@ MAX_INST = 10
 
 '''Configure Quantum Circuit properties'''
 MAX_QUBITS = 12
+MAX_QUBITS_CLASSIFICATION = 8
 MAX_REPS = 12
 MIN_REPS = 2
 MAX_IDENTITIES_BLOCKS = 3  # <---- Number of identity blocks, depth values of identity blocks is close to that of normal ansatz 
@@ -23,18 +24,18 @@ IDENTITY_BLOCKS_OVERLAY = 3
 ENTANGLEMENT = 'linear'
 
 '''Configure Measurement for Quantum Circuit, support different types of Operators, and their position'''
-GLOBAL_OPERATOR = SparsePauliOp.from_list([('Z'*MAX_QUBITS, 1)])
+GLOBAL_OPERATOR = SparsePauliOp.from_list([('Z'*MAX_QUBITS_CLASSIFICATION, 1)])
 
 # How many Qubits to be measured
 LOCAL_MEASUREMENT = 2
 
 # [...IIZZ]
-LOCAL_OPERATOR_BOTTOM = SparsePauliOp.from_list([('I' * (MAX_QUBITS - LOCAL_MEASUREMENT)+'Z'*LOCAL_MEASUREMENT, 1)])
+LOCAL_OPERATOR_BOTTOM = SparsePauliOp.from_list([('I' * (MAX_QUBITS_CLASSIFICATION - LOCAL_MEASUREMENT)+'Z'*LOCAL_MEASUREMENT, 1)])
 
 # [ZZII...]
-LOCAL_OPERATOR_TOP = SparsePauliOp.from_list([('Z'*LOCAL_MEASUREMENT+'I' * (MAX_QUBITS - LOCAL_MEASUREMENT), 1)])
+LOCAL_OPERATOR_TOP = SparsePauliOp.from_list([('Z'*LOCAL_MEASUREMENT+'I' * (MAX_QUBITS_CLASSIFICATION - LOCAL_MEASUREMENT), 1)])
 
 # [...IZZI...]
-LOCAL_OPERATOR_MIDDLE = SparsePauliOp.from_list([('I'*(np.floor(MAX_QUBITS/2)-np.floor(LOCAL_MEASUREMENT/2)).astype(int)+'Z'*LOCAL_MEASUREMENT + 'I'*(MAX_QUBITS-(np.floor(MAX_QUBITS/2)-np.floor(LOCAL_MEASUREMENT/2)).astype(int)-LOCAL_MEASUREMENT),1)])
+LOCAL_OPERATOR_MIDDLE = SparsePauliOp.from_list([('I'*(np.floor(MAX_QUBITS_CLASSIFICATION/2)-np.floor(LOCAL_MEASUREMENT/2)).astype(int)+'Z'*LOCAL_MEASUREMENT + 'I'*(MAX_QUBITS-(np.floor(MAX_QUBITS_CLASSIFICATION/2)-np.floor(LOCAL_MEASUREMENT/2)).astype(int)-LOCAL_MEASUREMENT),1)])
 
 LOG_PATH = './Logs-Cancer-v4'
